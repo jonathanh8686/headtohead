338de1d925da9ab7589c86f1427c85347ce4fc19
Michael Chen <mdchenmail@gmail.com>
2022-01-04 23:04:13 -0800

    color stuff

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 
 
 export default function Title(props) {
-    
-
+    var settings = "text-9xl bg-[#115D7E] text-white"
+    var lightmode = true
     return (
-        <div className="text-9xl bg-red-600 text-white">
+        <div className={lightmode ? settings : settings}>
             Head to Head!
         </div>
     )
