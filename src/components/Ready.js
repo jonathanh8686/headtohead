6f36502e54644fe7bfa51f84c096550a8f232045
Michael Chen <mdchenmail@gmail.com>
2022-01-14 19:59:52 -0800

    leader stuff

--- src/components/Ready.js
+++ src/components/Ready.js
@@ -4,7 +4,8 @@ export default function Ready(props){
     var settings = "flex-1 bg-[#FFFFFF] rounded-2xl p-2"
     return props.isLeader ? (
         <div className = {settings}>
-            start!
+            <button>  start! </button>
+           
         </div>
     ) : (
         <div className = {settings}>
