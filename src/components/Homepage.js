41fe8fcf64e35fc20b5605e02eb65a105e665fe4
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-01-16 16:31:38 -0800

    fixed scrolling issue

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,9 +8,10 @@ import RoomAlert from './RoomAlert'
 export default function Homepage(props) {
     
     return (
-        <div className="flex-col bg-[#343a44]">
-            <div><Navbar/></div>
-            <div className="min-h-screen text-center mt-5">
+        <div className="min-h-screen flex-col bg-[#343a44]">
+            <div><Navbar/></div> 
+            {/* TODO: rename navbar */}
+            <div className="text-center mt-5">
                 <Joinroom/>
             </div>
             <div className=""><RoomAlert error={props.error} message={props.message}/></div>
