191e937a5cb08ee137eb573a7a91795b0b5601a3
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-01-17 23:11:42 -0800

    did i stage that

--- src/components/LobbySettings.js
+++ src/components/LobbySettings.js
@@ -4,40 +4,22 @@ import Lobby from "./Lobby";
 export default function LobbySettings(props) {
     var games = ["game1", "game2", "game3"]
 
-    const untoggledButton = (gameName) => {
+    const gameButton = (gameName, toggled) => {
         return (
-            <button className="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded" key={gameName}
-                onClick={props.isLeader ? () => { props.setGameSelect(gameName) } : () => { }}>
+            <button className={(toggled ? "bg-blue-500 text-white" : "bg-transparent text-blue-700") + " font-semibold py-2 px-4 border border-blue-500 rounded"} key={gameName}
+                onClick={() => {
+                    if(!props.isLeader) return;
+                    toggled ? props.setGameSelect('') : props.setGameSelect(gameName)
+                }}>
                 {gameName}
             </button>
-        )
-    }
-
-
-    const toggledButton = (gameName) => {
-        return (
-            <button className="bg-blue-500 text-white font-semibold py-2 px-4 border border-blue-500 rounded" key={gameName}
-                onClick={props.isLeader ? () => { props.setGameSelect('') } : () => { }}>
-                {gameName}
-            </button>
-        )
-    }
-
-    const gameDisplay = (gameName) => {
-        return (
-            <div className="bg-blue-500">
-                {gameName}
-            </div>
-
         )
     }
 
     return (
         <div className="flex-row h-screen justify-center space-x-2 bg-[#FFFFFF] rounded-2xl p-2">
             {
-                games.map((game) => game == props.gameSelect ?
-                    toggledButton(game) :
-                    untoggledButton(game))
+                games.map((game) => gameButton(game, game==props.gameSelect))
             }
         </div>
     )
