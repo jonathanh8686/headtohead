fe20f62f92b9fb1ef2e1d7614e8672044942a4ef
Michael Chen <mdchenmail@gmail.com>
2022-01-17 17:34:26 -0800

    player list leader

--- src/components/lobby/PlayerList.js
+++ src/components/lobby/PlayerList.js
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import StarIcon from '@mui/icons-material/Star';
+//import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 
 export default function PlayerList(props){
     var settings = "flex-col-1 h-full w-full bg-[#FFFFFF] rounded-2xl p-2"
+    const leaderEntry = (nickname, key) => {
+        console.log(`leader entry generated nickname:${nickname} index:${key}`);
+        return (
+            <div className="flex bg-[#FFFFFF] hover:bg-[#DADADA]" key={key}>
+                <div>{nickname}</div>
+                <StarIcon/>
+            </div>
+        )
+    }
+    const otherEntry = (nickname, key) => {
+        console.log('other entry generated');
+        return (
+            <div className="flex bg-[#FFFFFF] hover:bg-[#DADADA]" key={key}>
+                <div>{nickname}</div>
+                <KeyboardDoubleArrowUpIcon/>
+            </div>
+        )
+    }
+    
     return(
         <div className = {settings}>
-            {props.content.map(nickname => <div key={nickname}>{nickname}</div>)}
+            {/*props.content.map((nickname,index) => {
+                (index == 0) ? leaderEntry(nickname,index) : otherEntry(nickname,index);
+            })*/}
+            {props.content.map(nickname=> <div key={nickname}>{nickname}</div>)}
+            
+            
         </div>
     )
 }
