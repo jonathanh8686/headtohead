109312f415bee69a542c98c4d082891e9d615dcd
Michael Chen <mdchenmail@gmail.com>
2022-01-22 15:31:24 -0800

    scoe stuff

--- src/components/games/ReactionTime/ReactionTimeGame.js
+++ src/components/games/ReactionTime/ReactionTimeGame.js
@@ -7,8 +7,10 @@ export default function ReactionTimeGame() {
     const [greenTime, setGreenTime] = useState(undefined);
     const [showButton, setShowButton] = useState(true);
     const [score, setScore] = useState(0);
-    const [falseStarted, setFalseStarted] = useState(false);
-    const [doubleFalseStarted, setDoubleFalseStarted] = useState(false);
+    const [baseTime, setBaseTime] = useState(0);
+    const [falseStartCount, setFalseStartCount] = useState(0)
+
+    const falseStartPenalty = 50;
 
     socket.on('reactionTimeStart', () => {
         setIsGreen(true);
@@ -22,6 +24,15 @@ export default function ReactionTimeGame() {
         }
     }, []);
 
+    useEffect(() => {
+        if(isGreen){
+            setScore(baseTime + falseStartPenalty * falseStartCount);
+            socket.emit("reactionTimeResult", {
+                "score": baseTime + falseStartPenalty * falseStartCount,
+            })
+            setShowButton(false);
+        }
+    },[baseTime])
     return (
         <div className='flex justify-center items-center h-full'>
             {showButton ?
@@ -30,25 +41,30 @@ export default function ReactionTimeGame() {
                         className={(isGreen ? "bg-munsellgreen" : "bg-skyblue") + " w-1/2 h-1/2 rounded-xl drop-shadow-2xl text-7xl font-[Bebas_Neue] p-5"}
                         onMouseDown={() => {
                             if(!isGreen) {
-                                socket.emit('processFalseStart');
+                                //socket.emit('processFalseStart');
+                                setFalseStartCount(falseStartCount + 1);
                                 return;
                             }
-                            setScore(Date.now() - greenTime);
-                            socket.emit("reactionTimeResult", {
-                                "score": score,
-                            })
-                            setShowButton(false);
+                            setBaseTime(Date.now() - greenTime);
+                            
+                            
+                            
+                            
                         }}>
                         {isGreen ? "Go!" : "Click once green..."}
-                        <span className='text-2xl'>
-                            <br></br>
-                            {falseStarted && !isGreen ? "You false started! The next false start will disqualify you!" : ""}
-                        </span>
+                        
                     </button>
                 ) :
                 (
-                    <div className={(doubleFalseStarted ? 'bg-honey' : 'bg-cornflowerblue') + ' grid justify-items-center items-center w-1/2 h-1/2 rounded-xl drop-shadow-2xl text-7xl font-[Bebas_Neue] p-5'}>
-                        {doubleFalseStarted ? "You were disqualified for a double false start!" : (score + " ms")}
+                    <div className='flex flex-col bg-cornflowerblue grid justify-items-center items-center w-1/2 h-1/2 rounded-xl drop-shadow-2xl text-7xl font-[Bebas_Neue] p-5'>
+                        <div>{(score + " ms")}</div>
+                        <div className='text-3xl'> 
+                            <span className='text-green-500'> {baseTime} </span>
+                            <span> + </span>
+                            <span className='text-red-500'> {falseStartPenalty * falseStartCount} </span>
+                            
+                        </div>
+                        
                     </div>
                 )
             }
